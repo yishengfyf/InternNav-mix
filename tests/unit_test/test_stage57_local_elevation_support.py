8d80a3165b6e1b8d41c9ad3c70fffaa209e88bbd
yifeifeng <fengyfct@163.com>
2026-08-30 17:43:42 +0800

    Fix Stage57 leading segment test fixture

--- tests/unit_test/test_stage57_local_elevation_support.py
+++ tests/unit_test/test_stage57_local_elevation_support.py
@@ -96,13 +96,13 @@ def test_local_support_graph_distinguishes_leading_from_later_safe_segment():
     memory = _Memory()
     del memory.occ_counts[(10, 10, 0)]
     del memory.occ3d_frame_counts[(10, 10, 0)]
-    for col in range(14, 18):
+    for col in range(14, 21):
         key = (10, col, 0)
         memory.occ_counts[key] = 4
         memory.occ3d_frame_counts[key] = 2
     report = _module.audit_local_elevation_support(
         memory,
-        [[10, 10], [10, 17]],
+        [[10, 10], [10, 20]],
         footprint_radius_m=0.0,
         max_step_up_m=0.06,
         max_step_down_m=0.06,
